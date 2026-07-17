f5c075a2721e0a61875fddf6c612e90dde1c7682
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2026-07-17 17:14:21 +0530

    Update page.tsx

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("This was added from GitHub");
   return (
     <div className="border-2 border-red-600 text-white p-5 flex flex-col justify-center items-center gap-6 rounded-3xl ">
       <div className="flex flex-col gap-1">
         <h1 className="font-bold text-2xl">Welcome To Contact Manager</h1>
-        <p className="font-medium text-lg">Manage your contacts easily and securly</p>
+        <p className="font-medium text-lg">Manage your contacts easily and with safety</p>
       </div>
       {/* image */}
       <Image src="/next.svg" alt="home-image" className="invert" width={300} height={300} priority />
