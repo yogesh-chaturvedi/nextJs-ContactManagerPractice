84f7b15c6f0ae0bea2be82ee52335809504aee59
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2026-07-22 17:39:41 +0530

    change heading size

--- my-next-app/src/app/contact/page.tsx
+++ my-next-app/src/app/contact/page.tsx
@@ -7,7 +7,7 @@ import ContactList from "../_component/ContactList";
 const contactPage = async () => {
 
     const user = await getSession();
-    console.log(user)
+    // console.log(user)
     if (!user) {
         return <div className="flex text-white flex-col justify-center items-center gap-5">
             <Link href={"/login"}>Please login</Link>
@@ -15,14 +15,12 @@ const contactPage = async () => {
     }
 
     const userContacts = await getContacts()
-    console.log("userContacts", userContacts)
-
-
+    // console.log("userContacts", userContacts)
 
     return (
         <div className="text-white flex flex-col gap-5 items-center justify-center">
 
-            <h1 className="font-bold text-2xl ">Contact Page</h1>
+            <h1 className="font-bold text-4xl text-blue-500 ">Contact Page</h1>
 
             <Link href={"/contact/new"} className="bg-gray-700 border-2 border-gray-700 rounded-2xl p-2">Add contact +</Link>
 
